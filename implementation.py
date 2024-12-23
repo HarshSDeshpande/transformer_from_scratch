e0bc6180326b7eab36b4f6624c6cc647dc6ccdc2
Time-11-emiT <harshdeshpande2010@gmail.com>
2024-12-23 18:49:52 +0530

    Added MLP :

--- implementation.py
+++ implementation.py
@@ -192,3 +192,26 @@ if MAIN:
     rand_float_test(Attention,[2,4,768])
     load_gpt2_test(Attention,reference_gpt2.blocks[0].attn,cache["blocks.0.ln1.hook_normalized"])
 # %%
+class MLP(nn.Module):
+    def __init__(self,cfg):
+        super().__init__()
+        self.cfg = cfg
+        self.W_in = nn.Parameter(torch.empty((cfg.d_model,cfg.d_mlp)))
+        nn.init.normal_(self.W_in,std=self.cfg.init_range)
+        self.b_in = nn.Parameter(torch.zeros(cfg.d_mlp))
+        self.W_out = nn.Parameter(torch.empty((cfg.d_mlp,cfg.d_model)))
+        nn.init.normal_(self.W_out,std=self.cfg.init_range)
+        self.b_out = nn.Parameter(torch.zeros(cfg.d_model))
+
+    def forward(self, normalized_resid_mid):
+        if self.cfg.debug:
+            print("Normalized_resid+mid:", normalized_resid_mid.shape)
+        pre = einsum("batch position d_model, d_model d_mlp -> batch position d_mlp", normalized_resid_mid, self.W_in) + self.b_in
+        post = gelu_new(pre)
+        mlp_out = einsum("batch position d_mlp, d_mlp d_model -> batch position d_model", post, self.W_out) + self.b_out
+        return mlp_out
+# %%
+if MAIN:
+    rand_float_test(MLP,[2,4,768])
+    load_gpt2_test(MLP,reference_gpt2.blocks[0].mlp,cache["blocks.0.ln2.hook_normalized"])
+# %%
